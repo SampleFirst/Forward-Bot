94818dbc52c1596e59364c3a786459a4a38ca02a
Sample First <132287607+SampleFirst@users.noreply.github.com>
2024-03-05 23:12:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
 import logging
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN
+from info import API_ID, API_HASH, BOT_TOKEN
 
 logging.getLogger().setLevel(logging.INFO)
-
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            session=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
